dc61ed66d4e95e71b87135cca946e19b2efd491d
Slim <you@example.com>
2024-11-10 23:51:09 +0500

    generics

--- src/generics/generics_for_method/ArrayUtils.java
+++ src/generics/generics_for_method/ArrayUtils.java
@@ -1,9 +1,14 @@
 package generics.generics_for_method;
 
+import java.lang.reflect.Array;
+
 public class ArrayUtils {
     public static <T> void swap(T[] array, int index1, int index2) {
         T temp = array[index1];
         array[index1] = array[index2];
         array[index2] = temp;
+    }
+    public <K> K[] swap1(K[] array) {
+        return array;
     }
 }
